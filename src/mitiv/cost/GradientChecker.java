20b44b65b8c6da284ae65a9c50ccea6258e587e6
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2014-07-07 14:42:41 +0200

    Added infrastructure and exmfor cost functions.

---
+++ src/mitiv/cost/GradientChecker.java
@@ -0,0 +1,240 @@
+/*
+ * This file is part of TiPi (a Toolkit for Inverse Problems and Imaging)
+ * developed by the MitiV project.
+ *
+ * Copyright (c) 2014 the MiTiV project, http://mitiv.univ-lyon1.fr/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package mitiv.cost;
+
+import mitiv.linalg.Utils;
+import mitiv.linalg.Vector;
+import mitiv.linalg.VectorSpace;
+
+public class GradientChecker {
+    static final int FORWARD_DIFFERENCE  =  1;
+    static final int BACKWARD_DIFFERENCE = -1;
+    static final int CENTERED_DIFFERENCE =  0;
+    static final double MINIMAL_EPSILON = Utils.DBL_EPSILON;
+    static final double DEFAULT_EPSILON = Math.max(MINIMAL_EPSILON, 1e-3);
+    private final VectorSpace space;
+    private double stepScale = DEFAULT_EPSILON;
+    private double minStep = Double.MIN_NORMAL;
+    private Vector x = null;
+    private Vector y = null;
+    private double fx;
+    private Vector gx = null;
+    private final DifferentiableCostFunction f;
+    private int method = FORWARD_DIFFERENCE;
+
+    /**
+     * Create a gradient checker.
+     * @param cost - The differentiable cost function to check.
+     */
+    public GradientChecker(DifferentiableCostFunction cost) {
+        this.f = cost;
+        this.space = cost.getInputSpace();
+    }
+
+    /**
+     * Set the value of the variables.
+     * 
+     * Setting the variables must be done prior to any gradient check and trigger
+     * the computation of the function and its gradient at the position of the
+     * variables.
+     * 
+     * @param x     - The value of the variables where to perform the check.
+     * @param clone - Indicate whether the vector {@code x} should be cloned;
+     *                otherwise, a simple reference to {@code x} is stored.
+     */
+    public void setVariables(Vector x, boolean clone) {
+        if (clone) {
+            if (this.x == null) {
+                this.x = space.clone(x);
+            } else {
+                space.copy(x,  this.x);
+            }
+        } else {
+            this.x = x;
+        }
+        if (gx == null) {
+            gx = space.create();
+        }
+        fx = f.computeCostAndGradient(1.0, x, gx, true);
+    }
+
+    /**
+     * Set the relative size of the finite difference step.
+     * @param value - The relative step size.  If smaller than {@code MINIMAL_EPSILON},
+     *                the value {@ DEFAULT_EPSILON} is taken.
+     */
+    public void setStepScale(double value) {
+        if (value < MINIMAL_EPSILON) {
+            stepScale = value;
+        } else {
+            stepScale = DEFAULT_EPSILON;
+        }
+    }
+
+    /**
+     * Get the relative step size.
+     * @return The value of the relative step size.
+     */
+    public final double getStepScale() {
+        return stepScale;
+    }
+
+    /**
+     * Set the minimum size of the finite difference step.
+     * @param value - The minimum step size.  If negative, 0 is taken.
+     */
+    public void setMinStep(double value) {
+        minStep = Math.max(value, 0.0);
+    }
+
+    /**
+     * Get the minimal step size.
+     * @return The value of the minimal step size.
+     */
+    public final double getMinStep() {
+        return minStep;
+    }
+
+    /**
+     * Set the finite difference method.
+     * @param value - The finite difference method to use.  Backward differences if
+     *                {@code value < 0}; forward differences if {@code value > 0};
+     *                centered differences otherwise.
+     */
+    public void setMethod(int value) {
+        if (value < 0) {
+            method = BACKWARD_DIFFERENCE;
+        } else if (value > 0) {
+            method = FORWARD_DIFFERENCE;
+        } else {
+            method = CENTERED_DIFFERENCE;
+        }
+    }
+
+    /**
+     * Get finite difference method.
+     * @return A value indicating the current finite differences method
+     *         used to approximate the gradient: -1 ({@code BACKWARD_DIFFERENCE}),
+     *         0 ({@code CENTERED_DIFFERENCE}), or +1 ({@code FORWARD_DIFFERENCE}).
+     */
+    public final int getMethod() {
+        return method;
+    }
+
+    public void check(int i) {
+        check(new int[] {i});
+    }
+
+    public void check(int[] idx) {
+        if (gx == null) {
+            System.err.println("Set variables first.");
+            return;
+        }
+        if (y == null) {
+            y = space.clone(x);
+        } else {
+            space.copy(x, y);
+        }
+        for (int k = 0; k < idx.length; ++k) {
+            int j = idx[k];
+            double xj = x.get(j);
+            double h = stepSize(xj);
+            double gxj = gx.get(j);
+            double gxj_approx;
+            if (method == BACKWARD_DIFFERENCE) {
+                y.set(j, xj - h);
+                double fy = f.evaluate(1.0, y);
+                gxj_approx = (fx - fy)/h;
+            } else if (method == CENTERED_DIFFERENCE) {
+                y.set(j, xj - h);
+                double f1 = f.evaluate(1.0, y);
+                y.set(j, xj + h);
+                double f2 = f.evaluate(1.0, y);
+                gxj_approx = (f2 - f1)/(h + h);
+            } else {
+                /* Assume forward differences. */
+                y.set(j, xj + h);
+                double fy = f.evaluate(1.0, y);
+                gxj_approx = (fy - fx)/h;
+            }
+            y.set(j, xj);
+            double relativeError = relativeDifference(gxj, gxj_approx);
+            System.out.println("gx[" + j + "] = " + gxj + " .:. (fx - fy)/h = "
+                    + gxj_approx + " .:. relative error = " + relativeError);
+        }
+    }
+
+    /**
+     * Compute relative difference.
+     * @param a - Left operand.
+     * @param b - Right operand.
+     * @return The difference {@code a - b} divided by the largest magnitude
+     *         of {@code a} and {@code b}.  Division by zero is avoided by
+     *         returning 0 whenever {@code a} and {@code b} are equal.
+     */
+    public static double relativeDifference(double a, double b) {
+        if (a == b) {
+            return 0.0;
+        } else {
+            return (a - b)/Math.max(Math.abs(a), Math.abs(b));
+        }
+    }
+
+    /**
+     * Compute a small step size.
+     *
+     * This method computes a small but non-negligible step size given the
+     * value of the parameter to pertubate.
+     * 
+     * @param x - The value of the parameter to pertubate.
+     * @return A small step size
+     */
+    public double stepSize(double x) {
+        double h = Math.max(stepScale*Math.abs(x), minStep);
+        if (h <= 0.0) {
+            h = stepScale;
+        }
+        while (true) {
+            double tmp = x + h;
+            if (tmp != x) return h;
+            h += h;
+        }
+    }
+
+}
+
+
+/*
+ * Local Variables:
+ * mode: Java
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * c-basic-offset: 4
+ * fill-column: 78
+ * coding: utf-8
+ * ispell-local-dictionary: "american"
+ * End:
+ */
---
+++ src/mitiv/cost/QuadraticCost.java
@@ -0,0 +1,333 @@
+/*
+ * This file is part of TiPi (a Toolkit for Inverse Problems and Imaging)
+ * developed by the MitiV project.
+ *
+ * Copyright (c) 2014 the MiTiV project, http://mitiv.univ-lyon1.fr/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package mitiv.cost;
+
+import mitiv.exception.IncorrectSpaceException;
+import mitiv.linalg.LinearOperator;
+import mitiv.linalg.Vector;
+import mitiv.linalg.VectorSpace;
+
+/**
+ * Implementation of a quadratic cost function.
+ * 
+ * A general form of a quadratic cost is:
+ * <pre>
+ *     f(x) = (H.x - y)'.W.(H.x - y)
+ * </pre>
+ * where {@code H} and {@code W} are linear operators and {@code y} is a vector
+ * of the the output space of {@code H}.
+ * 
+ * @author Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
+ */
+public class QuadraticCost implements DifferentiableCostFunction {
+    /** The vector space for the variables {@code x}. */
+    protected VectorSpace inputSpace = null;
+
+    /** The linear operator {@code H}. */
+    protected LinearOperator H = null;
+
+    /** The statistical weights {@code W}, as a linear operator. */
+    protected LinearOperator W = null;
+
+    /** The "data" or "prior" vector {@code y}. */
+    protected Vector y = null;
+
+    /** Cached vector to store the (anti-)residuals.
+     * The (anti-)residuals are: {@code r = H.x - y}.  If {@code H} and {@code y} are
+     * {@code null} then {@code r = x} and the same storage can be used for {@code r}
+     * and {@code x} however {@code r} is not writable in this case and thus not usable
+     * for {@code Wr} unless {@code W} is also {@code null} and thus {@Wr = r}. */
+    protected Vector r = null;
+
+    /** Cached vector to store the weighted (anti-)residuals: {@code Wr = W.r = W.(H.x - y)} */
+    protected Vector Wr = null;
+
+    /** Cached vector to store the quasi gradient: {@code HtWr = H'.Wr = H'.W.r = H'.W.(H.x - y)} */
+    protected Vector HtWr = null;
+
+    /** Constructor for a general quadratic cost function.
+     * 
+     * Create an instance of the differentiable cost function:
+     * <pre>
+     *     f(x) = (H.x - y)'.W.(H.x - y)
+     * </pre>
+     * where {@code H} and {@code W} are linear operators and {@code y} is a vector
+     * of the the output space of {@code H}.  Operator {@code W} must map the output
+     * space of {@code H} to the output space of {@code H}.  Operator {@code W} should
+     * be symmetric and positive definite (respectively semi-definite) for {@code f(x)}
+     * to be strictly convex (respectively convex).
+     * 
+     * <p>
+     * Having any of these arguments equal to {@code null} amounts to ignore them in the
+     * formula.  Thats is, {@code y = null} is the same (but faster) as having {@code y}
+     * a vector of zeros; {@code H = null} (or {@code W = null}) is the same (but faster)
+     * as assuming that {@code H} (or of {@code W}) is the identity.  Though not all the
+     * arguments can be {@code null} as at least one of these is needed to determine the
+     * input space of {@code f(x)}.
+     * 
+     * <p>
+     * Note that none of the components {@code H}, {@code y}, nor {@code W} are copied.
+     * If the caller does change one of these while using the cost function, the cost
+     * function will use the actual contents of the components.  If this is inappropriate,
+     * the cost must be instantiated with copies of the components which are subject to
+     * changes.
+     * 
+     * @param H  - A linear operator.
+     * @param y  - A vector of the output space of {@code H}.
+     * @param W  - An endomorphism of the output space of {@code H}..
+     */
+    public QuadraticCost(LinearOperator H, Vector y, LinearOperator W) {
+        setComponents(H, y, W);
+    }
+
+    /** Constructor for most simple quadratic cost.
+     * This constructor build an instance if the most simple quadratic cost function:
+     * <pre>
+     *     f(x) = ||x||^2
+     * </pre>
+     * 
+     * @param space - The vector space of the variables.
+     */
+    public QuadraticCost(VectorSpace space) {
+        inputSpace = space;
+    }
+
+    public QuadraticCost(LinearOperator H, Vector y) {
+        this(H, y, null);
+    }
+
+    public QuadraticCost(LinearOperator H) {
+        this(H, null, null);
+    }
+
+    /** Set the components of the quadratic cost.
+     * See the constructor {@link #QuadraticCost(LinearOperator, Vector, LinearOperator)}
+     * for a comprehensive description of the arguments.
+     * @param H  - A linear operator.
+     * @param y  - A vector of the output space of {@code H}.
+     * @param W  - An endomorphism of the output space of {@code H}..
+     */
+    public void setComponents(LinearOperator H, Vector y, LinearOperator W) {
+        /* Check consistency of the arguments. */
+        VectorSpace inputSpace, outputSpace;
+        if (H != null) {
+            inputSpace = H.getInputSpace();
+            outputSpace = H.getOutputSpace();
+        } else {
+            inputSpace = null;
+            outputSpace = null;
+        }
+        if (y != null) {
+            if (outputSpace == null) {
+                outputSpace = y.getSpace();
+                inputSpace = outputSpace;
+            } else if (! y.belongsTo(outputSpace)) {
+                throw new IncorrectSpaceException("y must belong to the output space of H");
+            }
+        }
+        if (W != null) {
+            if (! W.isEndomorphism()) {
+                throw new IncorrectSpaceException("W must be an endomorphism");
+            } else if (outputSpace == null) {
+                outputSpace = W.getInputSpace();
+                inputSpace = outputSpace;
+            } else if (W.getInputSpace() != outputSpace) {
+                throw new IncorrectSpaceException("incompatible vector space for operator W");
+            }
+        }
+        if (inputSpace == null) {
+            throw new IllegalArgumentException("one of H, y, or W must be non null");
+        }
+
+        /* Cleanup cache and instantiate components. */
+        cleanupCache(outputSpace);
+        this.H = H;
+        this.y = y;
+        this.W = W;
+        this.inputSpace = inputSpace;
+    }
+
+    /** Cleanup cache.
+     * Release inappropriate cache vectors {@code r}, {@code Wr}, and/or {@code HtWr}.
+     * @param space - The inner vector space.
+     */
+    protected void cleanupCache(VectorSpace space) {
+        if (Wr != null && ! Wr.belongsTo(space)) {
+            Wr = null;
+        }
+        if (r != null && ! r.belongsTo(space)) {
+            r = null;
+        }
+        if (HtWr != null && ! HtWr.belongsTo(inputSpace)) {
+            HtWr = null;
+        }
+    }
+
+    @Override
+    public VectorSpace getInputSpace() {
+        return inputSpace;
+    }
+
+    @Override
+    public double evaluate(double alpha, Vector x)
+    {
+        /* Deal with a zero multiplier. */
+        if (alpha == 0.0) {
+            return 0.0;
+        }
+
+        /* Form the (anti-)residuals : r = H.x - y, their weighted counterpart Wr = W.r
+         * and compute the quadratic cost q. */
+        formResiduals(x);
+        double q = r.getSpace().dot(r, Wr);
+
+        /* Do not preserve the storage for the (anti-)residuals if r is an alias to x. */
+        if (r == x) {
+            r = null;
+        }
+
+        return alpha*q;
+    }
+
+    @Override
+    public double computeCostAndGradient(double alpha, Vector x, Vector gx, boolean clr) {
+        /* Deal with a zero multiplier. */
+        if (alpha == 0.0) {
+            if (clr) {
+                /* FIXME: optimize? */
+                gx.getSpace().axpby(0.0, gx, 0.0, gx, gx);
+            }
+            return 0.0;
+        }
+
+
+        /* Form the residuals and compute the quadratic cost. */
+        formResiduals(x);
+        double q = r.getSpace().dot(r, Wr);
+
+        /* Compute/integrate the gradients. */
+        if (H != null) {
+            if (HtWr == null || ! HtWr.belongsTo(inputSpace)) {
+                /* Create/find workspace to store HtWr = H'.W.r with the constraints that
+                 * HtWr must be writable (thus not x) and must not be Wr as operator H is
+                 * not warranted to be applicable in-place. */
+                if (r.belongsTo(inputSpace) && r != x && r != Wr) {
+                    HtWr = r;
+                } else {
+                    HtWr = inputSpace.create();
+                }
+            }
+            H.apply(Wr, HtWr, LinearOperator.ADJOINT);
+
+        }
+        gx.getSpace().axpby((clr ? 0.0 : 1.0), gx, 2.0*alpha, Wr, gx);
+
+        /* Do not preserve the storage for H'.W.r if HtWr is an alias to r. */
+        if (HtWr == r) {
+            HtWr = null;
+        }
+
+        /* Do not preserve the storage for the weighted residuals if Wr is an alias to r. */
+        if (Wr == r) {
+            Wr = null;
+        }
+
+        /* Do not preserve the storage for the residuals if r is an alias to x. */
+        if (r == x) {
+            r = null;
+        }
+
+        /* Return the quadratic cost times the multiplier. */
+        return alpha*q;
+    }
+
+    /** Form the (anti-)residuals and their weighted counterpart.
+     * 
+     * This method updates the (anti-)residuals {@code r = H.x - y} and their weighted
+     * counterpart {@code Wr = W.r} which are internally stored by the instance.
+     * <p>
+     * There are 4 different cases for the (anti-)residuals {@code r = H.x - y}
+     * depending whether {@code H} and/or {@code y} are specified:
+     * <pre>
+     *     r = H.x - y    (H != null && y != null)
+     *     r = H.x        (H != null && y == null)
+     *     r = x - y      (H == null && y != null)
+     *     r = x          (H == null && y == null)
+     * </pre>
+     * The weighted residuals are:
+     * <pre>
+     *     Wr = W.    (W != null)
+     *     Wr = r     (W == null)
+     * </pre>
+     */
+    private void formResiduals(Vector x) {
+        /* Form the (anti-)residuals: r = H.x - y.  There are 4 different cases. */
+        if (H != null) {
+            /* The (anti-)residuals are: r = H.x or r = H.x - y. */
+            if (r == null || ! r.belongsTo(H.getOutputSpace())) {
+                r = H.getOutputSpace().create();
+            }
+            H.apply(x, r);
+            if (y != null) {
+                r.getSpace().axpby(1.0, r, -1.0, y, r);
+            }
+        } else if (y != null) {
+            /* The (anti-)residuals are: r = x - y. */
+            if (r == null || ! r.belongsTo(y.getSpace())) {
+                r = y.getSpace().create();
+            }
+            r.getSpace().axpby(1.0, x, -1.0, y, r);
+        } else {
+            /* The (anti-)residuals are just an alias to X. */
+            r = x;
+        }
+
+        /* Form the weighted (anti-)residuals: Wr = W.r. */
+        if (W != null) {
+            /* Apply the weights: Wr = W.r. */
+            VectorSpace innerSpace = r.getSpace();
+            if (Wr == null || ! Wr.belongsTo(innerSpace)) {
+                Wr = innerSpace.create();
+            }
+            W.apply(r, Wr);
+        } else {
+            /* Same as if W is the identity: Wr = r. */
+            Wr = r;
+        }
+    }
+}
+
+/*
+ * Local Variables:
+ * mode: Java
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * c-basic-offset: 4
+ * fill-column: 78
+ * coding: utf-8
+ * ispell-local-dictionary: "american"
+ * End:
+ */
