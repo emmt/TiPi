e626509253e1ba95ed7a952fc60fc277f4cb165f
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2014-07-28 12:34:39 +0200

    View classes removed

--- src/mitiv/base/view/autogen.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-for src in FloatView*.java; do
-    for Typ in Double Long Int Short Byte; do
-        typ=$(echo $Typ | tr [[:upper:]] [[:lower:]])
-        dst=$(echo "$src" | sed "s/^Float/${Typ}/")
-        sed "s/float/${typ}/g;s/Float/${Typ}/g" <"$src" >"$dst"
-    done
-done
