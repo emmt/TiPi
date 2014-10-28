4e803bf25040cca7d2556183afae9122c40aad16
Wir3n <tsi.beaubras@gmail.com>
2014-10-28 12:49:49 +0100

    modif

--- src/plugins/mitiv/conv/Convolution.java
+++ src/plugins/mitiv/conv/Convolution.java
@@ -26,6 +26,8 @@
 package plugins.mitiv.conv;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -35,6 +37,7 @@ import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.xml.crypto.Data;
 
 import mitiv.array.Double2D;
 import mitiv.array.Double3D;
@@ -75,8 +78,15 @@ public class Convolution extends EzPlug implements EzStoppable,SequenceListener,
     EzVarFile   varFileIMAGE;
     EzVarSequence EzVarSequencePSF;
     EzVarSequence EzVarSequenceImage;
+    EzVarSequence EzVarSequenceSave;
     JSlider slider;
     int sliderValue = 3;
+    int w;
+    int h;
+    int d;
+    double[] x;
+    double[] psf;
+    DoubleShapedVector y;
 
     String[] filters = {"no kernel", "average", "disk", "sobel", "prewitt", "kirsh"};
     String noNoise = "no noise";
@@ -97,13 +107,14 @@ public class Convolution extends EzPlug implements EzStoppable,SequenceListener,
     public static final int SOBEL = 5;
     public static final int KIRSH = 6;
     public static final int DISK = 7;
-    
+
 
     @Override
     protected void initialize()
     {
         EzVarSequenceImage = new EzVarSequence("Image");
         EzVarSequencePSF = new EzVarSequence("Load PSF");
+
         noise = new EzVarText("Noise", new String[] {noNoise, gaussian}, 0, false);
         slider = new JSlider(0, 100, sliderValue);
         slider.setEnabled(false);  
@@ -118,6 +129,27 @@ public class Convolution extends EzPlug implements EzStoppable,SequenceListener,
         super.addEzComponent(EzVarSequenceImage);
         super.addEzComponent(EzVarSequencePSF);
         super.addEzComponent(noise);
+        /* Objet deconvolution button */
+        EzButton saveMDAButton = new EzButton("Save to mda", new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                saveMDA();
+            }
+        });
+
+        EzButton loadSomething = new EzButton("Load data", new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                loaData();
+            }
+        });
+
+        addEzComponent(saveMDAButton);
+        addEzComponent(loadSomething);
         super.addComponent(slider);
         super.addComponent(label);
     }
@@ -128,34 +160,85 @@ public class Convolution extends EzPlug implements EzStoppable,SequenceListener,
         if (myseq != null) {
             myseq.close();
         }
-        
+
         Sequence seqImg = EzVarSequenceImage.getValue();
         Sequence seqPSF = EzVarSequencePSF.getValue();
 
-        int w = seqImg.getSizeX();
-        int h = seqImg.getSizeY();
-        int d = seqImg.getSizeZ();
 
-        double[] x = new double[w*h*d];
-        double[] psf = new double[w*h*d];
+        this.w = seqImg.getSizeX();
+        this.h = seqImg.getSizeY();
+        this.d = seqImg.getSizeZ();
+
+        this.x = new double[w*h*d];
+        this.psf = new double[w*h*d];
         for(int k = 0; k < d; k++)
         {
             Array1DUtil.arrayToDoubleArray(seqImg.getDataXY(0, k, 0), 0, x, k*w*h, w*h, seqImg.isSignedDataType());
             Array1DUtil.arrayToDoubleArray(seqPSF.getDataXY(0, k, 0), 0, psf, k*w*h, w*h, seqImg.isSignedDataType());
         }
-        
-            double[] psf_shift= MathUtils.fftShift3D(psf, w, h, d);
-            DoubleShapedVector y = MathUtils.convolution(x, psf_shift, w, h, d);
-            
-            Sequence seqY = new Sequence();
-            seqY.setName("Y");
-            for(int k = 0; k < d; k++)
-            {
-                seqY.addImage(new IcyBufferedImage(w, h, MathUtils.getArray(y.getData(), w, h, k)));
-            }
-            addSequence(seqY);
+
+        double[] psf_shift= MathUtils.fftShift3D(psf, w, h, d);
+        y = MathUtils.convolution(x, psf_shift, w, h, d);
+
+        Sequence seqY = new Sequence();
+        seqY.setName("Y");
+        for(int k = 0; k < d; k++)
+        {
+            seqY.addImage(new IcyBufferedImage(w, h, MathUtils.getArray(y.getData(), w, h, k)));
+        }
+        addSequence(seqY);
 
     }
+
+    private void saveMDA()
+    {
+        System.out.println(w);
+        MathUtils.stat(x);
+        DoubleArray xArray =  Double3D.wrap(x, w, h, d);
+        DoubleArray psfArray =  Double3D.wrap(psf, w, h, d);
+        DoubleArray yArray = Double3D.wrap(y.getData(), w, h, d);
+        try {
+            //MdaFormat.save(xArray, "/home/ludo/Images/TestShrinkBars/barShrink_mda");
+            MdaFormat.save(psfArray, "/home/ludo/Images/TestShrinkBars/psfGL_mda");
+            MdaFormat.save(yArray, "/home/ludo/Images/TestShrinkBars/barShrinkBlurredGL_mda");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void loaData()
+    {
+
+            DoubleArray data;
+            try {
+                data = MdaFormat.load("/home/ludo/Images/TestShrinkBars/barShrink_mda").toDouble();
+                Sequence seqD = new Sequence();
+                seqD.setName("Y");
+                for(int k = 0; k < 64; k++)
+                {
+                    seqD.addImage(new IcyBufferedImage(128, 128, MathUtils.getArray(data.flatten(), 128, 128, k)));
+                }
+                addSequence(seqD);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (DataFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (RecoverableFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+    }
+
 
     @Override
     public void clean()
