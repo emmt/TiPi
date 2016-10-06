bf072c293846b705dda997d0db235f3e654d89cb
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-10-06 22:27:27 +0200

    Add deprecated suggestion

--- src/mitiv/base/Shape.java
+++ src/mitiv/base/Shape.java
@@ -26,13 +26,11 @@
 //
 package mitiv.base;
 
-
 /**
- * Shape object to store dimesnions list.
+ * Shape object to store list of dimensions.
  * <p>
- * A Shape object is unmodifiable and is used to store the dimensions of a
- * shaped object in an efficient way.  To preserve its properties, Shape
- * objects can only be built by factories (static methods).
+ * A Shape object is immutable and is used to store the dimensions of a
+ * shaped object in an efficient way.
  * </p>
  * @author Éric Thiébaut
  */
@@ -52,6 +50,7 @@ public class Shape {
     /**
      * Get the shape of scalar objects.
      * @return The shape of a scalar object.
+     * @deprecated Use {@link #scalarShape} instead.
      */
     @Deprecated
     public static Shape make() {
@@ -64,6 +63,7 @@ public class Shape {
      *               an array of length equals to {@code 0} and yields the
      *               shape of a scalar object).
      * @return A new shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int[])} instead.
      */
     @Deprecated
     public static Shape make(int[] dims) {
@@ -80,6 +80,7 @@ public class Shape {
      *               an array of length equals to {@code 0} and yields the
      *               shape of a scalar object).
      * @return A new shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(long[])} instead.
      */
     @Deprecated
     public static Shape make(long[] dims) {
@@ -94,6 +95,7 @@ public class Shape {
      * Make a 1-D shape
      * @param dim1 - The 1st dimension.
      * @return A new 1-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1) {
@@ -105,6 +107,7 @@ public class Shape {
      * @param dim1 - The 1st dimension.
      * @param dim2 - The 2nd dimension.
      * @return A new 2-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2) {
@@ -117,6 +120,7 @@ public class Shape {
      * @param dim2 - The 2nd dimension.
      * @param dim3 - The 3rd dimension.
      * @return A new 3-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3) {
@@ -130,6 +134,7 @@ public class Shape {
      * @param dim3 - The 3rd dimension.
      * @param dim4 - The 4th dimension.
      * @return A new 4-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4) {
@@ -144,6 +149,7 @@ public class Shape {
      * @param dim4 - The 4th dimension.
      * @param dim5 - The 5th dimension.
      * @return A new 5-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4, int dim5) {
@@ -159,6 +165,7 @@ public class Shape {
      * @param dim5 - The 5th dimension.
      * @param dim6 - The 6th dimension.
      * @return A new 6-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4, int dim5, int dim6) {
@@ -175,6 +182,7 @@ public class Shape {
      * @param dim6 - The 6th dimension.
      * @param dim7 - The 7th dimension.
      * @return A new 7-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4, int dim5, int dim6, int dim7) {
@@ -192,6 +200,7 @@ public class Shape {
      * @param dim7 - The 7th dimension.
      * @param dim8 - The 8th dimension.
      * @return A new 8-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int,int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4, int dim5, int dim6, int dim7, int dim8) {
@@ -210,6 +219,7 @@ public class Shape {
      * @param dim8 - The 8th dimension.
      * @param dim9 - The 9th dimension.
      * @return A new 9-dimensional shape built from the given dimensions.
+     * @deprecated Use constructor {@link #Shape(int,int,int,int,int,int,int,int,int)} instead.
      */
     @Deprecated
     public static Shape make(int dim1, int dim2, int dim3, int dim4, int dim5, int dim6, int dim7, int dim8, int dim9) {
