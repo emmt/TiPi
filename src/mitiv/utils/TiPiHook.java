97924197f5ddebc7afdb06e44ec79091ce5be2db
ferreol <ferreol.soulez@free.fr>
2017-05-22 18:31:42 +0200

    Few more comments

--- src/mitiv/utils/TiPiHook.java
+++ src/mitiv/utils/TiPiHook.java
@@ -4,6 +4,8 @@
 package mitiv.utils;
 
 /**
+ * Define a hook that can be called during computation.
+ * It is usefull to show information / image during iteration
  * @author ferreol
  *
  */
@@ -11,8 +13,8 @@ public abstract interface TiPiHook {
 
     /**
      * Run specific code
-     * @param caller
-     * @param iter
+     * @param caller   calling object
+     * @param iter     number of current iteration
      */
     public abstract void run(Object caller, int iter);
 }
