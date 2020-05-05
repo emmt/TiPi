66d7e83a02cd7601724665d97ef4ad345623c1b6
ferreol <ferreol.soulez@free.fr>
2020-05-05 17:18:37 +0200

    Add a generic PsfModel class

---
+++ src/mitiv/psf/PsfModel.java
@@ -0,0 +1,57 @@
+/**
+ *
+ */
+package mitiv.psf;
+
+import mitiv.array.ShapedArray;
+import mitiv.base.Shape;
+import mitiv.linalg.shaped.DoubleShapedVector;
+
+/**
+ * @author ferreol
+ *
+ */
+public abstract class PsfModel {
+
+    protected boolean single = false;
+    protected Shape psfShape;
+    protected ShapedArray psf; //3D point spread function
+
+
+
+    /**
+     * Launch internal routines to compute PSF
+     */
+    abstract public void computePsf();
+
+    /**
+     * @return the PSF
+     */
+    abstract public ShapedArray getPsf();
+
+    /**
+     * @return the MTF
+     */
+    abstract public ShapedArray getMtf();
+
+    /**
+     * Setter for PSF parameters. The parameter type is given by the parameter space of @param
+     * @param param PSF parameters
+     */
+    abstract public void setParam(DoubleShapedVector param);
+
+
+    /**
+     * Free some memory
+     */
+    abstract public void freeMem();
+
+    /**
+     * Setter for the single precision flag
+     * @param single
+     */
+    public void setSingle(boolean single){
+        this.single = single;
+    }
+
+}
